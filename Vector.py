b7fb9915b8fe50ffa292d4f266046e52229974d3
Shuvamoy Mondal <shuvamoymondal@Shuvamoys-MacBook-Pro.local>
2018-09-02 22:47:10 +0530

    Added vector

---
+++ Vector.py
@@ -0,0 +1,14 @@
+# Load library
+import numpy as np
+
+# Create vector
+vector = np.array([1, 2, 3, 4, 5, 6])
+
+# Create matrix
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+
+print(vector.transpose())
+print(matrix.transpose())
